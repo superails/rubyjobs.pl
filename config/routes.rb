b13e95e163752a1d4c0f8252fa7b0d7b89cfa131
Marcin Doliwa <mdoliwa@gmail.com>
2019-08-09 21:54:17 +0200

    Add jobs controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'jobs/index'
+  get 'jobs/new'
+  resources :jobs, only: [:index, :new]
+  root to: 'jobs#index'
 end
