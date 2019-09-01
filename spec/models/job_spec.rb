715a0c679fec537b383e7a6f5b972bc6092832ea
Marcin Doliwa <mdoliwa@gmail.com>
2019-09-01 22:57:01 +0200

    Add tests for job's location= method

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -18,4 +18,26 @@ RSpec.describe Job, type: :model do
       expect(job.locations.map(&:name)).to_not include('Zdalnie')
     end
   end
+
+  describe "#location=" do
+    it 'assigns locations with given names to job' do
+      location = create(:location, name: 'Warszawa')
+
+      job = Job.new
+
+      job.location = "Warszawa, Białystok"
+
+      expect(job.locations.map(&:name)).to eq ["Warszawa", "Białystok"]
+    end
+
+    it 'assigns locations from db when exist with given name' do
+      location = create(:location, name: 'Warszawa')
+
+      job = Job.new
+
+      job.location = "Warszawa, Białystok"
+
+      expect(job.locations.map(&:persisted?)).to eq [true, false]
+    end
+  end
 end
