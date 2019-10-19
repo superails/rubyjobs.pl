09616c6a742a83e06ff3d4036364a7c08feaf3e0
Marcin Doliwa <mdoliwa@gmail.com>
2019-10-19 14:14:21 +0200

    Register apply link clicks for published job offers only

--- app/controllers/job_offers/redirects_controller.rb
+++ app/controllers/job_offers/redirects_controller.rb
@@ -2,7 +2,7 @@ class JobOffers::RedirectsController < ApplicationController
   def show
     @job_offer = JobOffer.find(params[:id])
 
-    RegisterApplyLinkClickJob.perform_later(@job_offer.id)
+    RegisterApplyLinkClickJob.perform_later(@job_offer.id) if @job_offer.published?
 
     redirect_to @job_offer.apply_link
   end
--- spec/controllers/job_offers/redirects_controller_spec.rb
+++ spec/controllers/job_offers/redirects_controller_spec.rb
@@ -1,20 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe JobOffers::RedirectsController, type: :controller do
-  before :each do 
-    ActiveJob::Base.queue_adapter = :test
-  end
-
-  after :each do 
-    ActiveJob::Base.queue_adapter = :inline
-  end
-
   describe 'GET #show' do
-    it 'enqueues RegisterApplyLinkClickJob' do
-      job_offer = create(:job_offer, apply_link: 'https://rubyjobs.pl/careers/ruby-developer')
-
-      expect { get :show, params: {id: job_offer.id} }.to have_enqueued_job(RegisterApplyLinkClickJob).with(job_offer.id)
+    before :each do 
+      ActiveJob::Base.queue_adapter = :test
     end
+
+    after :each do 
+      ActiveJob::Base.queue_adapter = :inline
+    end
+
+    context 'when job_offer is published' do
+      it 'enqueues RegisterApplyLinkClickJob' do
+        job_offer = create(:job_offer, published_at: Time.zone.now, apply_link: 'https://rubyjobs.pl/careers/ruby-developer')
+
+        expect { get :show, params: {id: job_offer.id} }.to have_enqueued_job(RegisterApplyLinkClickJob).with(job_offer.id)
+      end
+    end
+
+    context 'when job_offer is not published' do
+      it 'does not enqueue RegisterJobOfferVisitJob' do
+        job_offer = create(:job_offer, published_at: nil, apply_link: 'https://rubyjobs.pl/careers/ruby-developer')
+
+        expect { get :show, params: {id: job_offer.id} }.to_not have_enqueued_job(RegisterApplyLinkClickJob).with(job_offer.id)
+      end
+    end
+
   end
 end
 
