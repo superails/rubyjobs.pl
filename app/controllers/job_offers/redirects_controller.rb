23953f8c9d800744b1e5880f4fa7e3d6b77ba600
Marcin Doliwa <mdoliwa@gmail.com>
2019-10-20 20:17:55 +0200

    Fix redirecting to email address

--- app/controllers/job_offers/redirects_controller.rb
+++ app/controllers/job_offers/redirects_controller.rb
@@ -4,6 +4,10 @@ class JobOffers::RedirectsController < ApplicationController
 
     RegisterApplyLinkClickJob.perform_later(@job_offer.id) if @job_offer.published?
 
-    redirect_to @job_offer.apply_link
+    if @job_offer.apply_link =~ URI::MailTo::EMAIL_REGEXP
+      redirect_to "mailto:#{@job_offer.apply_link}"
+    else
+      redirect_to @job_offer.apply_link
+    end
   end
 end
