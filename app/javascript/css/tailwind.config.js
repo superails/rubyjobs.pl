4e2cf946c07748d488fa1c83d26d49e4f2cbc255
Marcin Doliwa <mdoliwa@gmail.com>
2020-02-13 21:28:43 +0100

    Upgrade tailwindcss and add custom forms node module

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -1,7 +1,31 @@
 module.exports = {
   theme: {
-    extend: {}
+    customForms: theme => ({
+      default: {
+        checkbox: {
+          iconColor: theme('borderColor.red[600]'),
+          '&:checked': {
+            backgroundColor: theme('colors.white'),
+            borderColor: theme('borderColor.red[600]'),
+            borderWidth: theme('borderWidth[2]'),
+            backgroundColor: theme('colors.red[100]'),
+          },
+          '&:focus': {
+            boxShadow: 'none',
+            borderColor: theme('transparent'),
+          },
+        },
+      },
+    }),
+    extend: {
+      fontFamily: {
+        header: ['Roboto', 'sans-serif'],
+        body: ['"Open Sans"', 'sans-serif']
+      }
+    }
   },
   variants: {},
-  plugins: []
+  plugins: [
+    require('@tailwindcss/custom-forms'),
+  ]
 }
